97bd7b457b99ce9cc2fc4f68e3cba1b72af89e23
bordonz <rodrigozbordon@gmail.com>
2026-06-12 12:18:21 -0300

    Fix: Corrección en la ruta principal(/) para que redirija al home

--- routes/imagen.js
+++ routes/imagen.js
@@ -3,6 +3,10 @@ import { mostrarPublicacion } from '../controller/imagen.js';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.redirect('/galeria');
+});
+
 router.get('/galeria', mostrarPublicacion)
 
 export default router;
